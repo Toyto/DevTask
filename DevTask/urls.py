c49d9f12bc4b8313a765d9b86d0e749d02c16848
System Administrator <wertuk2@gmail.com>
2016-01-25 20:03:20 +0200

    add products pagex

--- DevTask/urls.py
+++ DevTask/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from product.views import ProductsView
 
-urlpatterns = [
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DevTask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^products/', ProductsView.as_view(), name='products'),
+)
--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Product
 
 # Create your views here.
+class ProductsView(TemplateView):
+    template_name = 'product/products.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductsView, self).get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
